3dbc899adb5ac3c8b0bd64f56677ede8d18621bf
Khadim Fall <khadimfall0503@gmail.com>
2025-05-17 14:17:15 +0000

    Application Web en marche

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index,liste_produits,detail_produits
 
 urlpatterns = [
-    path('', index, name='index' )
+    path('', index, name='index' ),
+    path('produits/', liste_produits, name='liste_produits' ),
+    path('detail/<int:id>/', detail_produits, name='detail_produits'),
+
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
 
 def index(request):
     product_objects = Product.objects.all()
     context = {'product_objects': product_objects}
     return render(request, 'shop/index.html', context)
+
+def liste_produits(request):
+    produits = Product.objects.all()
+    context = {'produits':produits}
+    return render (request, 'shop/liste_produits.html', context)
+
+def detail_produits(request,id):
+    detail = get_object_or_404(Product,id=id)
+    context = {'detail':detail}
+    return render(request,'shop/detail_produits.html',context)
