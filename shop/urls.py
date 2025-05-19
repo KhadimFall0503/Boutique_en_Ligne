94b6597de2e35923d0bf382d562ffa346c3995f3
Khadim Fall <khadimfall0503@gmail.com>
2025-05-19 15:14:10 +0000

    Site Réussi avec Succés

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index,liste_produits,detail_produits
+from .views import index,liste_produits,detail_produits,login_views,search_results,cart_detail,add_to_cart,cart_view,remove_from_cart
 
 urlpatterns = [
     path('', index, name='index' ),
     path('produits/', liste_produits, name='liste_produits' ),
     path('detail/<int:id>/', detail_produits, name='detail_produits'),
+   path('ajouter/<int:product_id>/', add_to_cart, name='add_to_cart'),
+   path('cart/', cart_view, name='cart_view'),
+   path('cart/remove/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
+    path('search/', search_results, name='search_results'),
+     path('panier/', cart_detail, name='cart_detail'),
+
 
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
+from .forms import LoginForm
+from django.shortcuts import redirect
+
 
 def index(request):
     product_objects = Product.objects.all()
@@ -15,3 +18,84 @@ def detail_produits(request,id):
     detail = get_object_or_404(Product,id=id)
     context = {'detail':detail}
     return render(request,'shop/detail_produits.html',context)
+
+def login_views(request):
+    form = LoginForm()
+
+    return render(request,'shop/ajouter.html')
+
+def search_results(request):
+    query = request.GET.get('q')
+    results = []
+    if query:
+        results = Product.objects.filter(title__icontains=query)
+    return render(request, 'shop/search_results.html', {'results': results, 'query': query})
+
+def some_view(request):
+    cart = request.session.get('cart', {})  # ex: {'product_id': quantity, ...}
+    cart_count = sum(cart.values())  # total des quantités
+    # Puis passer dans le contexte
+    return render(request, 'template.html', {'cart_count': cart_count})
+
+def cart_detail(request):
+    cart = request.session.get('cart', {})
+    products = Product.objects.filter(id__in=cart.keys())
+    
+    cart_items = []
+    total = 0
+    for product in products:
+        quantity = cart[str(product.id)]
+        subtotal = product.price * quantity
+        cart_items.append({
+            'product': product,
+            'quantity': quantity,
+            'subtotal': subtotal
+        })
+        total += subtotal
+
+    return render(request, 'shop/cart_detail.html', {
+        'cart_items': cart_items,
+        'total': total
+    })
+ 
+ # views.py
+
+
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', {})
+
+    if str(product_id) in cart:
+        cart[str(product_id)] += 1
+    else:
+        cart[str(product_id)] = 1
+
+    request.session['cart'] = cart
+    return redirect(request.META.get('HTTP_REFERER', 'product_list'))  # redirige vers la page précédente
+
+
+
+def cart_view(request):
+    cart = request.session.get('cart', {})
+    cart_items = []
+    total = 0
+    for product_id, quantity in cart.items():
+        product = Product.objects.get(id=product_id)
+        subtotal = product.price * quantity
+        cart_items.append({
+            'product': product,
+            'quantity': quantity,
+            'subtotal': subtotal
+        })
+        total += subtotal
+
+    return render(request, 'shop/cart.html', {'cart_items': cart_items, 'total': total})
+
+def remove_from_cart(request, product_id):
+    cart = request.session.get('cart', {})
+    product_id_str = str(product_id)
+    if product_id_str in cart:
+        del cart[product_id_str]
+        request.session['cart'] = cart
+    return redirect('cart_detail')
+
+
